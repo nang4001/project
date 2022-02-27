c87c57fa5aa734b22c8b76d51fe48926a9adf938
nang4001 <96340016+nang4001@users.noreply.github.com>
2022-02-27 20:23:01 +0900

    second

--- pro27/src/main/java/com/myspring/pro27/member/controller/MemberControllerImpl.java
+++ pro27/src/main/java/com/myspring/pro27/member/controller/MemberControllerImpl.java
@@ -195,7 +195,7 @@ public class MemberControllerImpl   implements MemberController {
 	}
 	return mav;
 	}
-
+    
 	@Override
 	@RequestMapping(value = "/member/logout.do", method =  RequestMethod.GET)
 	public ModelAndView logout(HttpServletRequest request, HttpServletResponse response) throws Exception {
